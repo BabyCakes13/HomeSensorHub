a007ce962f607199d0ff2452d2d4dff1a94fc345
Alexandru-Sever Horin <alex.sever.h@gmail.com>
2020-05-14 20:41:06 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='HomeSensorHub',
+    version='0.1dev',
+    packages=['communication','sensors'],
+    long_description=open('README.md').read(),
+)
